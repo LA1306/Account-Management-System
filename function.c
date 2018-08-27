7beb99db8913fa09bfe709057b0b619ef495b753
anhle <lablueen@gmail.com>
2018-08-27 22:46:45 +0700

    add func-add-acc-full

--- function.c
+++ function.c
@@ -17,34 +17,36 @@ int AccAdd()
 	int iAccTypeIndex;
 	char cQuit = '0';
 
-	sAdd.iAccNumber = 0;
+/*	sAdd.iAccNumber = 0;
 	strcpy(sAdd.cAccName, "0");
 	strcpy(sAdd.cAccType, "0");
-
+*/
 	fpLog = fopen("Log.txt", "a+");
 
 //	puts("Enter today's date (mm/dd/yyyy):");
 //	scanf("%d/%d/%d", sAdd.iAddMonth, sAdd.iAddDay, sAdd.iAddYear);
 
-	puts("Enter the account number (6 number):");
+	printf("Enter the account number (6 number): ");
 	scanf("%d", &sAdd.iAccNumber);
 	while(fscanf(fpLog, "%d %s %s\n", &sExist.iAccNumber, sExist.cAccName, sExist.cAccType) != EOF)
 	{
 		if(sAdd.iAccNumber == sExist.iAccNumber)
 		{
-			puts("Account already exist! [Q]");
-			while (cQuit != 'Q')
+			printf("Account already exist! [Q] ");
+			while (cQuit != 'q')
 			{
 				scanf("%c", &cQuit);
 			}
+
 			return 0;
 		}
 	}
-/*
-	puts("Enter the name:");
+
+//	puts("Enter the name:");	// puts: tu dong xuong hang
+	printf("Enter the name: ");
 	scanf("%s", sAdd.cAccName);
 	
-	puts("Account type:\n\t 1: Platium\n\t 2: Gold\n\t 3: Silver \nSelect account type:");
+	printf("Account type:\n\t 1: Platium\n\t 2: Gold\n\t 3: Silver \nSelect account type: ");
 	scanf("%d", &iAccTypeIndex);
 	switch (iAccTypeIndex)
 	{
@@ -57,7 +59,7 @@ int AccAdd()
 		default:
 			break;
 	}
-*/
+
 	fprintf(fpLog, "%d %s %s\n", sAdd.iAccNumber, sAdd.cAccName, sAdd.cAccType);
 	fclose(fpLog);
 	puts("Account created successfully! [Q]");
@@ -71,14 +73,12 @@ int AccAdd()
 
 int main()
 {
-	puts("hi!\n");
-
-	FILE *fpLog;
+/*	FILE *fpLog;
 	fpLog = fopen("Log.txt", "w+");
 	fprintf(fpLog, "%d %s %s\n", 0, "0", "0");
 	fclose(fpLog);
-
+*/
 	AccAdd();
-	
+
 	return 0;
 }
