961e7c5ecf2eec03eb550f7aa046b7a36965eb51
anhle <lablueen@gmail.com>
2018-08-27 22:18:19 +0700

    add func-add-acc-number

--- function.c
+++ function.c
@@ -11,14 +11,22 @@ typedef struct
 int AccAdd()
 {
 	FILE *fpLog;
+
 	sInfo sAdd, sExist;
+
 	int iAccTypeIndex;
 	char cQuit = '0';
+
+	sAdd.iAccNumber = 0;
+	strcpy(sAdd.cAccName, "0");
+	strcpy(sAdd.cAccType, "0");
+
+	fpLog = fopen("Log.txt", "a+");
 
 //	puts("Enter today's date (mm/dd/yyyy):");
 //	scanf("%d/%d/%d", sAdd.iAddMonth, sAdd.iAddDay, sAdd.iAddYear);
 
-/*	puts("Enter the account number (6 number):");
+	puts("Enter the account number (6 number):");
 	scanf("%d", &sAdd.iAccNumber);
 	while(fscanf(fpLog, "%d %s %s\n", &sExist.iAccNumber, sExist.cAccName, sExist.cAccType) != EOF)
 	{
@@ -32,7 +40,7 @@ int AccAdd()
 			return 0;
 		}
 	}
-
+/*
 	puts("Enter the name:");
 	scanf("%s", sAdd.cAccName);
 	
@@ -49,21 +57,27 @@ int AccAdd()
 		default:
 			break;
 	}
-
+*/
 	fprintf(fpLog, "%d %s %s\n", sAdd.iAccNumber, sAdd.cAccName, sAdd.cAccType);
 	fclose(fpLog);
 	puts("Account created successfully! [Q]");
-	while (cQuit != 'Q')
+	while (cQuit != 'q')
 	{
 		scanf("%c", &cQuit);
 	}
-*/
+
 	return 0;
 }
 
 int main()
 {
 	puts("hi!\n");
+
+	FILE *fpLog;
+	fpLog = fopen("Log.txt", "w+");
+	fprintf(fpLog, "%d %s %s\n", 0, "0", "0");
+	fclose(fpLog);
+
 	AccAdd();
 	
 	return 0;
